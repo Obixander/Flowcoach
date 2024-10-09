59c95761d0dbd2913cc6cd95b4108f01baab9386
obixander <noahhauge@gmail.com>
2024-10-09 15:00:05 +0200

    Added parts of question repository

--- FlowCoach.DataAccess/Interfaces/IQuestionRepository.cs
+++ FlowCoach.DataAccess/Interfaces/IQuestionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlowCoach.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace FlowCoach.DataAccess.Interfaces
 {
     public interface IQuestionRepository
     {
+        List<Question> GetFirstFiveQuestions(QuestionType questionType);
+        List<Question> GetNextFiveQuestions(int lastQuestionId, QuestionType questionType);
+        List<Question> GetAllQuestionsByType(QuestionType questionType);
+        
     }
 }
---
+++ FlowCoach.DataAccess/Repositories/QuestionRepository.cs
@@ -0,0 +1,27 @@
+﻿using FlowCoach.DataAccess.Interfaces;
+using FlowCoach.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowCoach.DataAccess.Repositories
+{
+    public class QuestionRepository(DataContext context) : GenericRepository<Question>(context),IQuestionRepository
+    {
+        public List<Question> GetAllQuestionsByType(QuestionType questionType)
+        {
+            return context.Questions.Where(q => q.QuestionType == questionType).ToList();
+        }
+
+        public List<Question> GetFirstFiveQuestions(QuestionType questionType)
+        {
+        }
+
+        public List<Question> GetNextFiveQuestions(int lastQuestionId, QuestionType questionType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
